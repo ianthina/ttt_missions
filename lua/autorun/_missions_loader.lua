37d882c1839cdd98ba8a19c115d496125cf76517
ianthina <gynopalio@gmail.com>
2023-02-04 10:00:39 -0600

    fixed gamemode check

--- lua/autorun/_missions_loader.lua
+++ lua/autorun/_missions_loader.lua
@@ -1,4 +1,4 @@
-if GAMEMODE_NAME == terrortown then
+if engine.ActiveGamemode() == "terrortown" then
     AddCSLuaFile("missions_init/sh_missions.lua")
     include("missions_init/sh_missions.lua")
 end
